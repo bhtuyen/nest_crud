b602406c02e99140049a733f8f433b398adf43aa
Bùi Huy Tuyền <me.bhtuyen@gmail.com>
2025-07-28 20:40:21 +0700

    cập nhật phiên bản @prisma/client và prisma lên 6.12.0; điều chỉnh các trường trong schema.prisma để sử dụng @map cho các tên trường trong cơ sở dữ liệu

---
+++ prisma/migrations/20250728132145_mapping_column_name/migration.sql
@@ -0,0 +1,73 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `AuthorID` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `Content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `CreatedAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `ID` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `Title` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `UpdatedAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `CreatedAt` on the `RefreshToken` table. All the data in the column will be lost.
+  - You are about to drop the column `ExpiresAt` on the `RefreshToken` table. All the data in the column will be lost.
+  - You are about to drop the column `Token` on the `RefreshToken` table. All the data in the column will be lost.
+  - You are about to drop the column `UserID` on the `RefreshToken` table. All the data in the column will be lost.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `CreatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `Email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `ID` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `Name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `Password` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `UpdatedAt` on the `User` table. All the data in the column will be lost.
+  - Added the required column `author_id` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `content` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - The required column `id` was added to the `Post` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `expires_at` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `token` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - The required column `id` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "author_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Post_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
+);
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+CREATE TABLE "new_RefreshToken" (
+    "token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires_at" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "RefreshToken_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
+);
+DROP TABLE "RefreshToken";
+ALTER TABLE "new_RefreshToken" RENAME TO "RefreshToken";
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
