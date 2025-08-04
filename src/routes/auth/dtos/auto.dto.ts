fb5566708f058404d026576983033dda99ec437c
Bùi Huy Tuyền <me.bhtuyen@gmail.com>
2025-08-04 21:08:18 +0700

    thêm decorator Match vào RegisterBodyDTO để xác thực mật khẩu và xác nhận mật khẩu phải khớp nhau

--- src/routes/auth/dtos/auto.dto.ts
+++ src/routes/auth/dtos/auto.dto.ts
@@ -2,6 +2,7 @@ import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
 import { UserEntity } from '../entities/user.entity';
 import { Exclude } from 'class-transformer';
 import { User } from '@prisma/client';
+import { Match } from '@shared/decorators/match';
 
 export class LoginBodyDTO implements Pick<UserEntity, 'Email' | 'Password'> {
   @IsString()
@@ -15,6 +16,10 @@ export class LoginBodyDTO implements Pick<UserEntity, 'Email' | 'Password'> {
 export class RegisterBodyDTO extends LoginBodyDTO implements Pick<UserEntity, 'Email' | 'Password' | 'Name'> {
   @IsString()
   Name: string;
+
+  @Match('Password', {
+    message: 'Password and ConfirmPassword must match'
+  })
   @IsString()
   @IsStrongPassword()
   ConfirmPassword: string;
